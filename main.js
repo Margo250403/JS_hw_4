b6c9e78f1c517ca52ebd07d8b3674dec9175d1f0
Margo250403 <margoshakac@gmail.com>
2022-02-13 14:07:39 +0200

    start hw

---
+++ main.js
@@ -0,0 +1,17 @@
+var text = prompt("Введите строку: My <b>name</b> is <i>Alex</i>")
+
+var result = ""
+var isChar = false
+
+for(var char of text) {
+     if(char === "<" || ">"){
+          continue
+     }
+          isFilter = true
+     if()
+     {
+          result += char 
+     }
+}
+
+console.log(result)
